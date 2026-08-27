8c78d9b0af7bd599fcecb9884f1db1d19df819cd
kunal k <250872275+kunal6002@users.noreply.github.com>
2026-08-27 13:25:05 +0530

    Add insertion sort implementation in C++
    Implement insertion sort algorithm to sort an array of integers.

---
+++ practical 1/insertion sort.c
@@ -0,0 +1,39 @@
+#include <iostream>          #insertion sort
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements: ";
+  for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
